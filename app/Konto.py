e78e469f17a7f661488954a48a2ad3af47a81288
Łukasz Kulpaczyński <lukasz18kulpaczynski@gmail.com>
2024-10-09 20:38:05 +0200

    wszystkie features wraz z ukończonymi pozytywnie testami zrobione

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,31 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel, kod_rabatowy=None):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
+
+        if len(pesel) == 11:
+            self.pesel = pesel
+        else:
+            self.pesel = "niepoprawny pesel"
+
+        if self.czy_kod_poprawny(kod_rabatowy) and self.jaki_wiek(pesel):
+            self.saldo += 50
+
+    def czy_kod_poprawny(self, kod_rabatowy):
+        if kod_rabatowy is None:
+            return False
+        if kod_rabatowy.startswith("PROM_") and len(kod_rabatowy) == 8:
+            return True
+        return False
+
+    def jaki_wiek(self, pesel):
+        yob = int(pesel[0:2])
+        mob = int(pesel[2:4])
+
+        if mob > 12:
+            yob += 2000
+        else:
+            yob += 1900
+
+        return yob > 1960
